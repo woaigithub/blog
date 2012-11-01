499c0d9bd8133e2a90b90a1e73b3924fa95c58b1
woaigithub <jorden008@gmail.com>
2012-11-01 18:13:55 +0800

    v0.01

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Blog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.active_record.default_timezone = :local 
+    config.time_zone = 'Beijing' 
   end
 end
