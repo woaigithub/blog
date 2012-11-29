f4e108700035aa99ff189b380f7afe5157420ebf
woaigithub <jorden008@gmail.com>
2012-11-29 16:55:39 +0800

    add post, edit post

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -8,4 +8,37 @@ class Admin::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+  def new 
+    @post = Post.new
+  end
+
+  def create
+    @category = Category.find(params[:post][:category_id])
+    params[:post].delete(:category_id)
+    @post = @category.posts.build(params[:post])
+    if @post.save
+      
+      redirect_to admin_posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end 
+
+
+  def update
+    @category = Category.find(params[:post][:category_id])
+    params[:post].delete(:category_id)
+    @post = Post.find(params[:id])
+    @post.category = @category
+    if @post.update_attributes(params[:post])
+      redirect_to admin_posts_path
+    else
+      render :edit
+    end
+  end
 end
