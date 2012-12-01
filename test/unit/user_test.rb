199d21d79d8cda6ef19f71ef1853be7670732017
woaigithub <jorden008@gmail.com>
2012-12-01 11:47:01 +0800

    add unit test for user's nickname filed

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,32 +2,34 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
  
-  test "email invalid" do
-    user = users(:one)
-    assert user.invalid?, "email invalid"
+  test "user have valid nickname" do
+     user = User.new(:nickname => "nickname", :email => "jor@123.com", :password => "123", :password_confirmation => "123")
+     assert user.valid?, "user should have valid nickname"
   end 
+  
+  def test_user_nickname_length_equal_min_1
+    user = User.new(:nickname => "n", :email => "jor@123.com", :password => "123", :password_confirmation => "123")
+    assert user.valid?, "use should have valid nickname"
+  end
+    
+  def test_user_nickname_length_equal_max_30
+    user = User.new(:nickname => "m"*30, :email => "jor@123.com", :password => "123", :password_confirmation => "123")
+    assert user.valid?, "use should have valid nickname"
+  end
 
-  test "email invalid two" do
-    user=users(:two)
+  def  test_user_without_nickname 
+     user = User.new(:email => "jor@123.com", :password => "123", :password_confirmation => "123")
+     assert user.invalid?
+  end  
+
+  def  test_user_nickname_length_equal_31 
+    user = User.new(:nickname => "d"*31, :email => "jor@123.com", :password => "123", :password_confirmation => "123")
+    assert user.invalid?
+  end
+
+  def  test_user_nickname_length_more_than_31
+    user = User.new(:nickname => "nickname"*5, :email => "jor@123.com", :password => "123", :password_confirmation => "123")
     assert user.invalid?
   end
  
-  test "password do not match" do
-    user =users(:three)
-    assert user.invalid?
-  end
-
-  test "all fields are valid" do
-    user=User.new(:nickname=>"nickname",:email=>"sdf@13123.xo",
-                  :password=>"123456",
-                  :password_confirmation=>"123456")
-    assert user.valid?
-  end
-
-  test "password and password_confirmation do not match" do
-    user = User.new(:nickname=>"asdf",:email=>"asdfasdf",:password=>"123",
-                                      :password_confirmation=>"234")
-    assert user.invalid?
-  end
-
 end
