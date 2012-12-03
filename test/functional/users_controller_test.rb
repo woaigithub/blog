2d16d93e9969d6545e516e8b24f3eac0576a010d
woaigithub <jorden008@gmail.com>
2012-12-03 16:22:16 +0800

    add some functional test for users controller, test sign up process

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include FactoryGirl::Syntax::Methods
+
+  def test_should_be_200
+    get :new
+    assert_response(200)
+  end
+
+  def test_should_be_stay_new_template_when_you_submit_invalid_signup_user
+    user=FactoryGirl.build(:user_invalid_without_email)
+    post :create, user
+    
+    assert_template :new
+    assert_equal "sign up failed!", flash[:notice]
+  end
+
+  def test_should_be_successful_when_you_submit_valid_signup_user
+
+
+    user =FactoryGirl.build(:user_valid)
+    post :create, {:user=>{:email=>user.email,:nickname=>user.nickname,
+    	                   :password=>user.password,
+    	                   :password_confirmation => user.password_confirmation}}
+    assert_equal  "sign up successfully!",flash[:notice]
+    assert @controller.signed_in?
+
+  end
+
 end
