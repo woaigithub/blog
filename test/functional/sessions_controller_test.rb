da6fec7f68ed4cd59faf1c0e30689f304a6d2c1b
woaigithub <jorden008@gmail.com>
2012-12-03 13:29:51 +0800

    add post functional test for sessions controller

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -10,9 +10,21 @@ class SessionsControllerTest < ActionController::TestCase
 
   def test_should_be_get_new_status_code
     get :new
-    assert_response :success
+    
+    assert_response(200) 
     assert_not_nil assigns(:user)
   end
 
   
+  def test_should_route_from_signout_to_destroy
+    
+     assert_routing( {:path =>"/signout", :method => :delete} , 
+     	            {:controller =>"sessions", :action =>"destroy", :method => :delete})
+  
+  end
+  
+  def test_should_be_render_new_template_after_email_do_not_match_password
+    post :create,{:user=>{:email=>"",:password=>""}}
+    assert_template :new
+  end
 end
